0e83350c18278403511baa3e5cc38157c3686e66
Sergey Leonov <agrael@yandex.ru>
2020-05-30 20:24:11 +0300

    module2-task1: travis fixes

--- js/stat.js
+++ js/stat.js
@@ -42,36 +42,36 @@ function getMaximumNumber(array) {
 function renderPlayerStatistic(ctx, name, column, time, maximumScore) {
   ctx.fillStyle = '#000';
   ctx.fillText(time,
-    CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
-    CLOUD_POSITION_X + MAX_GRAPH_HEIGHT - TEXT_HEIGHT - time / maximumScore * MAX_GRAPH_HEIGHT);
+      CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
+      CLOUD_POSITION_X + MAX_GRAPH_HEIGHT - TEXT_HEIGHT - time / maximumScore * MAX_GRAPH_HEIGHT);
   ctx.fillText(name,
-    CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
-    CLOUD_HEIGHT);
+      CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
+      CLOUD_HEIGHT);
 
-  name === 'Вы'
-    ? ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-    : ctx.fillStyle = getBackgroundForGraph();
+  if (name === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = getBackgroundForGraph();
+  }
 
   ctx.fillRect(
-    CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
-    CLOUD_POSITION_X + MAX_GRAPH_HEIGHT - time / maximumScore * MAX_GRAPH_HEIGHT,
-    GRAPH_WIDTH,
-    time / maximumScore * MAX_GRAPH_HEIGHT);
+      CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
+      CLOUD_POSITION_X + MAX_GRAPH_HEIGHT - time / maximumScore * MAX_GRAPH_HEIGHT,
+      GRAPH_WIDTH,
+      time / maximumScore * MAX_GRAPH_HEIGHT);
 }
 
-function renderStatistics(canvas,
-                          playersNames,
-                          timeToCompleteLvl) {
+function renderStatistics(canvas, playersNames, timeToCompleteLvl) {
   var biggestScore = Math.round(getMaximumNumber(timeToCompleteLvl));
 
   renderCloud(canvas,
-    CLOUD_POSITION_X + 10,
-    CLOUD_POSITION_Y + 10,
-    'rgba(0, 0, 0, 0.7)');
+      CLOUD_POSITION_X + 10,
+      CLOUD_POSITION_Y + 10,
+      'rgba(0, 0, 0, 0.7)');
   renderCloud(canvas,
-    CLOUD_POSITION_X,
-    CLOUD_POSITION_Y,
-    '#fff');
+      CLOUD_POSITION_X,
+      CLOUD_POSITION_Y,
+      '#fff');
 
 
   canvas.fillStyle = '#000';
@@ -80,14 +80,10 @@ function renderStatistics(canvas,
   canvas.fillText('Список результатов:', CLOUD_POSITION_X + CLOUD_BORDER_GAP, CLOUD_POSITION_Y + CLOUD_BORDER_GAP + TEXT_HEIGHT * 3);
 
   playersNames.forEach(
-    function (player, index) {
-      renderPlayerStatistic(canvas,
-        player,
-        index,
-        Math.round(
-          timeToCompleteLvl[index]
-        ),
-        biggestScore);
-    }
+      function (player, index) {
+        renderPlayerStatistic(canvas, player, index, Math.round(timeToCompleteLvl[index]), biggestScore);
+      }
   );
 }
+
+renderStatistics();
