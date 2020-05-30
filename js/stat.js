dfd8e66617cb9dbd1ae2b1d06d39cc4af20e6f2a
Sergey Leonov <agrael@yandex.ru>
2020-05-30 20:08:29 +0300

    module2-task1: endgame pop-up is done

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,15 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_POSITION_X = 100;
 var CLOUD_POSITION_Y = 10;
 
+var TEXT_HEIGHT = 10;
+
 var COLUMN_GAP = 50;
 var GRAPH_WIDTH = 40;
-var MAX_GRAPH_HEIGH = 150;
+var MAX_GRAPH_HEIGHT = 150;
 var COLUMN_WIDTH = COLUMN_GAP + GRAPH_WIDTH;
 
 
@@ -15,28 +19,59 @@ function renderCloud(ctx, posX, posY, color) {
 }
 
 function getBackgroundForGraph() {
-  return 'rgba(0, 0, 255, ' + getRandomArbitrary(0.1, 1) +')';
+  return 'rgba(0, 0, 255, ' + getRandomArbitrary(0.1, 1) + ')';
 }
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function renderPlayerStatistic(ctx, name, column) {
+function getMaximumNumber(array) {
+  var number = array[0];
+
+  for (var i = 0; i < array.length; i++) {
+    if (number < array[i]) {
+      number = array[i];
+    }
+  }
+
+  return number;
+}
+
+function renderPlayerStatistic(ctx, name, column, time, maximumScore) {
   ctx.fillStyle = '#000';
-  ctx.fillText('4025', CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column, 90);
-  ctx.fillText(name, CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column, CLOUD_HEIGHT);
+  ctx.fillText(time,
+    CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
+    CLOUD_POSITION_X + MAX_GRAPH_HEIGHT - TEXT_HEIGHT - time / maximumScore * MAX_GRAPH_HEIGHT);
+  ctx.fillText(name,
+    CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
+    CLOUD_HEIGHT);
 
   name === 'Вы'
     ? ctx.fillStyle = 'rgba(255, 0, 0, 1)'
     : ctx.fillStyle = getBackgroundForGraph();
 
-  ctx.fillRect(CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column, 100, GRAPH_WIDTH, MAX_GRAPH_HEIGH);
+  ctx.fillRect(
+    CLOUD_POSITION_X + COLUMN_GAP + COLUMN_WIDTH * column,
+    CLOUD_POSITION_X + MAX_GRAPH_HEIGHT - time / maximumScore * MAX_GRAPH_HEIGHT,
+    GRAPH_WIDTH,
+    time / maximumScore * MAX_GRAPH_HEIGHT);
 }
 
-function renderStatistics(canvas, playersNames, timeToCompleteLvl) {
-  renderCloud(canvas, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(canvas, 100, 10, '#fff');
+function renderStatistics(canvas,
+                          playersNames,
+                          timeToCompleteLvl) {
+  var biggestScore = Math.round(getMaximumNumber(timeToCompleteLvl));
+
+  renderCloud(canvas,
+    110,
+    20,
+    'rgba(0, 0, 0, 0.7)');
+  renderCloud(canvas,
+    100,
+    10,
+    '#fff');
+
 
   canvas.fillStyle = '#000';
   canvas.font = '16px PT mono';
@@ -44,8 +79,14 @@ function renderStatistics(canvas, playersNames, timeToCompleteLvl) {
   canvas.fillText('Список результатов:', 120, 60);
 
   playersNames.forEach(
-    (player, index) => {
-      renderPlayerStatistic(canvas, player, index);
+    function (player, index) {
+      renderPlayerStatistic(canvas,
+        player,
+        index,
+        Math.round(
+          timeToCompleteLvl[index]
+        ),
+        biggestScore);
     }
   );
 }
